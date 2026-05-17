1373b19b5161fdf68be187dce1b1017552495763
Luidi <programadorlbk@gmail.com>
2026-05-17 15:46:48 -0300

    encode de fibonacciZeckendorfService.js

---
+++ backend/src/services/fibonacciZeckendorfService.js
@@ -0,0 +1,70 @@
+const encode = (texto) => {
+    //fibonacciZeckendorfValidacao.validarEncodeFibonacciZeckendorf(texto);
+    const comprimido = [];
+    const stopBit = 1;
+    
+    
+    for(let i = 0; i < texto.length; i++){
+        let numSequenciaFibonacci = [1, 2];
+        let simboloContidoNaSequencia = false;
+        let indice = 0;
+
+        const charCode = texto.charCodeAt(i);
+
+        while(numSequenciaFibonacci[indice] <= charCode){
+            if(numSequenciaFibonacci[indice] == charCode){
+                simboloContidoNaSequencia = true;
+                break;
+            }
+            numSequenciaFibonacci.push(numSequenciaFibonacci[indice] + numSequenciaFibonacci[indice+1]);
+            indice++;
+        }
+
+        if(simboloContidoNaSequencia){
+            numSequenciaFibonacci.pop();
+        }
+        else{
+            numSequenciaFibonacci.pop();
+            numSequenciaFibonacci.pop();
+        }
+        
+        let soma = 0;
+        let codeword = "";
+        while(soma != charCode){
+           for(let j = numSequenciaFibonacci.length - 1; j >= 0; j--){
+                if(j == numSequenciaFibonacci.length - 1){
+                    soma += numSequenciaFibonacci[j];
+                    codeword += "1";
+                    //j--; //pq nao tem como ser consecutivo
+                    continue;
+                }
+                
+                if((soma + numSequenciaFibonacci[j]) > charCode){
+                    codeword = "0" + codeword;
+                }
+               
+                else if((soma + numSequenciaFibonacci[j]) < charCode){
+                    soma += numSequenciaFibonacci[j];
+                    codeword = "1" + codeword;
+                }
+
+                else{
+                    soma += numSequenciaFibonacci[j];
+                    codeword = "1" + codeword;
+                    if(j > 0){
+                        for(j = j - 1; j >= 0; j--){
+                            codeword = "0" + codeword;
+                        }
+                    }
+                    break;
+                }
+                
+            }
+        }
+        codeword += stopBit;
+        comprimido.push(codeword);
+    }
+    return comprimido;
+}
+
+console.log(encode("A"));
